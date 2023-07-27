303ae90212e8abfc6c2005f49c887584a682f5fe
mb-tousif <meta.block.tousif@gmail.com>
2023-07-28 01:40:45 +0600

    Redux installed

--- src/Layout/Meta.tsx
+++ src/Layout/Meta.tsx
@@ -2,10 +2,10 @@ import Head from "next/head";
 export default function Meta() {
   return (
     <Head>
-      <meta name="description" content="Country Meats for fresh meat supplier." />
+      <meta name="description" content="PC Builder Ltd is the best PC supplier." />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" type="image/png" href="/favicon.png" />
-      <title>Country Meats Ltd</title>
+      <title>PC Builder Ltd</title>
     </Head>
   );
 }
