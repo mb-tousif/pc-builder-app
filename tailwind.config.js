37ce560fd0140afb7d38338d743dba5294865c83
mb-tousif <meta.block.tousif@gmail.com>
2023-08-03 22:56:35 +0600

    --

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./dist/src/**/*.{js,jsx,ts,tsx}"
+    "./public/index.html",
   ],
   theme: {
     extend: {
